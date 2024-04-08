6fea3092fa981032cd11e9e87c941e937ed25417
Hendrik Nieländer <hendrik@nielaender.de>
2024-04-08 23:54:27 +0200

    fix: zvm formula

--- Formula/zvm.rb
+++ Formula/zvm.rb
@@ -9,9 +9,21 @@ class Zvm < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
-  if OS.mac? && Hardware::CPU.intel?
+puts "OS.mac?: #{OS.mac?}"
+puts "OS.linux?: #{OS.linux?}"
+puts "Hardware::CPU.intel?: #{Hardware::CPU.intel?}"
+puts "Hardware::CPU.arm?: #{Hardware::CPU.arm?}"
+puts "Version: v#{version}"
+
+
+
+  if OS.mac? 
+    if Hardware::CPU.intel?
       url "https://github.com/hendriknielaender/zvm/releases/download/v#{version}/x86_64-macos-zvm.tar.gz"
       sha256 "e386032f79a1808d1c656dd028c97818e404124e8933f6d9387d7a4e47efefb9"
+    else Hardware::CPU.arm?
+      # url "https://..."
+      # sha256 "..."
     end
   elsif OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
@@ -22,7 +34,7 @@ class Zvm < Formula
       sha256 "05d75b136cbbd7512889ac97b58ef375b6d457dc3c459b884d2d3301ff9bb1aa"
     end
   else
-    odie "Unsupported platform. Please submit a bug report here: https://github.com/hendriknielaender/zvm/issues\n#{OS.report}"
+    odie "Unsupported platform. Please submit a bug report here: https://github.com/hendriknielaender/zvm/issues"
   end
 
   def install
