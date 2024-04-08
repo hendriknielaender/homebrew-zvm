ed0fd4f1c57cfc92016193407a1201b7fedb59d1
Hendrik Nieländer <hendrik@nielaender.de>
2024-04-08 23:58:42 +0200

    fix: add exe name

--- Formula/zvm.rb
+++ Formula/zvm.rb
@@ -9,18 +9,11 @@ class Zvm < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
-puts "OS.mac?: #{OS.mac?}"
-puts "OS.linux?: #{OS.linux?}"
-puts "Hardware::CPU.intel?: #{Hardware::CPU.intel?}"
-puts "Hardware::CPU.arm?: #{Hardware::CPU.arm?}"
-puts "Version: v#{version}"
-
-
-
   if OS.mac? 
     if Hardware::CPU.intel?
       url "https://github.com/hendriknielaender/zvm/releases/download/v#{version}/x86_64-macos-zvm.tar.gz"
       sha256 "e386032f79a1808d1c656dd028c97818e404124e8933f6d9387d7a4e47efefb9"
+      @@executable = "x86_64-macos-zvm"
     else Hardware::CPU.arm?
       # url "https://..."
       # sha256 "..."
@@ -29,16 +22,18 @@ puts "Version: v#{version}"
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/hendriknielaender/zvm/releases/download/v#{version}/aarch64-linux-zvm.tar.gz"
       sha256 "fa6f92b91e737ba54286594931364f617acba8f5627bb7bf5f6b3271563f8237"
+      @@executable = "aarch64-linux-zvm"
     elsif Hardware::CPU.intel?
       url "https://github.com/hendriknielaender/zvm/releases/download/v#{version}/x86_64-linux-zvm.tar.gz"
       sha256 "05d75b136cbbd7512889ac97b58ef375b6d457dc3c459b884d2d3301ff9bb1aa"
+      @@executable = "x86_64-linux-zvm"
     end
   else
     odie "Unsupported platform. Please submit a bug report here: https://github.com/hendriknielaender/zvm/issues"
   end
 
   def install
-    bin.install "zvm" => "zvm"
+    bin.install @@executable => "zvm"
   end
 
   test do
